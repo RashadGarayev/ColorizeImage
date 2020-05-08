4f9e7020e2a05d493f41e45e210b1d9f7e81e8b7
Rashad Garayev <pythonaz@yahoo.com>
2020-05-09 00:10:13 +0400

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+
+def Image(arg):
+    net = cv2.dnn.readNetFromCaffe('models/colorization_deploy_v2.prototxt',
+                                   'models/colorization_release_v2.caffemodel')
+    pts = np.load('models/pts_in_hull.npy')
+    class8 = net.getLayerId("class8_ab")
+    conv8 = net.getLayerId("conv8_313_rh")
+    pts = pts.transpose().reshape(2, 313, 1, 1)
+    net.getLayer(class8).blobs = [pts.astype("float32")]
+    net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+
+    image = cv2.imread(arg)
+    scaled = image.astype(np.float32) / 255.0
+    lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+    resized = cv2.resize(lab, (224, 224))
+    L = cv2.split(resized)[0]
+    L -= 50
+    net.setInput(cv2.dnn.blobFromImage(L))
+    ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+    ab = cv2.resize(ab, (image.shape[1], image.shape[0]))
+    L = cv2.split(lab)[0]
+    colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+    colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+    colorized = np.clip(colorized, 0, 1)
+    colorized = (255 * colorized).astype("uint8")
+    colorized = cv2.resize(colorized, (281,281))
+    colorized = cv2.imwrite(r'tmp/colorized.jpg', colorized)
+
+def ColorizeVideo(frame):
+    net = cv2.dnn.readNetFromCaffe('models/colorization_deploy_v2.prototxt',
+                                   'models/colorization_release_v2.caffemodel')
+    pts = np.load('models/pts_in_hull.npy')
+    class8 = net.getLayerId("class8_ab")
+    conv8 = net.getLayerId("conv8_313_rh")
+    pts = pts.transpose().reshape(2, 313, 1, 1)
+    net.getLayer(class8).blobs = [pts.astype("float32")]
+    net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+    frame = cv2.resize(frame, (400, 400))
+    scaled = frame.astype("float32") / 255.0
+    lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+    resized = cv2.resize(lab, (224, 224))
+    L = cv2.split(resized)[0]
+    L -= 50
+    net.setInput(cv2.dnn.blobFromImage(L))
+    ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+    ab = cv2.resize(ab, (frame.shape[1], frame.shape[0]))
+    L = cv2.split(lab)[0]
+    colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+    colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+    colorized = np.clip(colorized, 0, 1)
+    colorized = (255 * colorized).astype("uint8")
+    return colorized
